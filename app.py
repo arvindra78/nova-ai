557537f8240d11f9859ba90747c97b08d7eee6a7
Arvindra Rajput <arvindrarajput675@gmail.com>
2025-06-30 21:25:34 +0530

    model change

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import os
 app = Flask(__name__)
 genai.configure(api_key="AIzaSyC7iaJKq0BBUxTdOODLGUPpYZyu_0rV09c")
 
-model = genai.GenerativeModel("models/gemma-3-27b-it")
+model = genai.GenerativeModel("models/gemma-3-1b-it")
 
 @app.route("/")
 def home():
